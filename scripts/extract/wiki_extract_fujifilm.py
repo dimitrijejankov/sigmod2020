17232d5adffbe5313bc9628a1c1528cd60dfc17c
vladimirjankovcd <vlajkojjj@gmail.com>
2020-03-04 00:38:15 +0100

    fujifilm added models

---
+++ scripts/extract/wiki_extract_fujifilm.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+
+# Fujifilm
+# F-SERIES 
+
+
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+
+url = "https://en.wikipedia.org/wiki/Fujifilm_FinePix_F_series"
+
+req = urllib.request.urlopen(url)
+article = req.read().decode()
+    
+with open('Fuji.html', 'w') as fo:
+    fo.write(article)
+        
+article = open('Fuji.html').read()
+soup = BeautifulSoup(article, 'html.parser')
+tables = soup.find_all('table', class_='sortable')
+
+for table in tables:
+    
+    with open('../../currated_data/fujifilm-models.json', 'a') as fo:
+        for tr in table.find_all('tr'):
+            tds = tr.find_all('td')
+            if not tds:
+                continue
+            
+            tds = tr.find_all('td')
+            name = tds[0].text.strip()
+            if(len(name) > 7):
+                splitet_name = name.split("F")
+                while("" in splitet_name):
+                    splitet_name.remove("")
+                for name in splitet_name:
+                    model = "F"+name
+                    line = "[" + '"' + model + '","' + model[:1] + " "+ model[1:]+ '","' + model[:1] + "-"+ model[1:]  + '"],'
+                    print(line.upper(), file =fo)
+            else:
+                model = name
+                line = "[" + '"' + model + '","' + model[:1] + " "+ model[1:]+ '","' + model[:1] + "-"+ model[1:]  + '"],'
+                print(line.upper(), file = fo)
+
+os.remove("Fuji.html")
+
+# %% j-series 
+
+url = 'https://en.wikipedia.org/wiki/Fujifilm_FinePix_J_series'
+
+req = urllib.request.urlopen(url)
+article = req.read().decode()
+    
+with open('Fuji.html', 'w') as fo:
+    fo.write(article)
+        
+article = open('Fuji.html').read()
+soup = BeautifulSoup(article, 'html.parser')
+                    
+paragraphs = soup.find_all('ul')
+
+for paragraph in paragraphs:
+    with open('../../currated_data/fujifilm-models.json', 'a') as fo:
+        for lines in paragraph.find_all('li'):
+            name = lines.text.strip()
+            if name != "" and  name[0] == "J" :
+                if name.endswith(" (discontinued)"):
+                    name = name[:len(name)- len(" (discontinued)")]
+                if name[-1] == "]":
+                    name = name[:len(name)-3]
+                #print(name)
+                line = "[" + '"' + name + '","' + name[:1] + " "+ name[1:]+ '","' + name[:1] + "-"+ name[1:]  + '"],'
+                if name[:2] == "JX":
+                    line = "[" + '"' + name + '","' + name[:2] + " "+ name[2:]+ '","' + name[:2] + "-"+ name[2:]  + '"],'
+                if name[:2] == "JV":
+                    line = "[" + '"' + name + '","' + name[:2] + " "+ name[2:]+ '","' + name[:2] + "-"+ name[2:]  + '"],'
+                if name[:2] == "JZ":
+                    line = "[" + '"' + name + '","' + name[:2] + " "+ name[2:]+ '","' + name[:2] + "-"+ name[2:]  + '"],'
+                print(line.upper(), file = fo)
+                
+os.remove("Fuji.html")  
+
+# %% s-series
+
+url = 'https://en.wikipedia.org/wiki/Fujifilm_FinePix_S-series'
+
+req = urllib.request.urlopen(url)
+article = req.read().decode()
+    
+with open('Fuji.html', 'w') as fo:
+    fo.write(article)
+        
+article = open('Fuji.html').read()
+soup = BeautifulSoup(article, 'html.parser')
+                    
+paragraphs = soup.find_all('ul')
+
+for paragraph in paragraphs:
+    with open('../../currated_data/fujifilm-models.json', 'a') as fo:
+        for lines in paragraph.find_all('li'):
+            for name in lines.find_all('a'):
+                if not name:
+                    continue
+                name = name.text.strip()
+                if "Fujifilm FinePix " in name: 
+                    name = name.replace("Fujifilm FinePix ","")
+                    name = name.replace("cameras", "")
+                    name2 = "FinePix " + name
+                    
+                    line1 = "[" + '"' + name + '","' + name[:1] + " "+ name[1:]+ '","' + name[:1] + "-"+ name[1:]  + '",'
+                    line2 =  '"' + name2 + '","' + name2[:9] + " "+ name2[9:]+ '","' + name2[:9] + "-"+ name2[9:]  + '"],'
+
+                    if name[:2] == "HS":
+                        line1 = "[" + '"' + name + '","' + name[:2] + " "+ name[2:]+ '","' + name[:2] + "-"+ name[2:]  + '",'
+                        line2 ='"' + name2 + '","' + name2[:10] + " "+ name2[10:]+ '","' + name2[:10] + "-"+ name2[10:]  + '"],'
+                    if name[:2] == "SL":
+                        line1 = "[" + '"' + name + '","' + name[:2] + " "+ name[2:]+ '","' + name[:2] + "-"+ name[2:]  + '",'
+                        line2 =  '"' + name2 + '","' + name2[:10] + " "+ name2[10:]+ '","' + name2[:10] + "-"+ name2[10:]  + '"],'
+                    print((line1 + line2).upper(), file=fo)
+                
+os.remove("Fuji.html")  
+# %% HS series, T - series
+with open('../../currated_data/fujifilm-models.json', 'a') as fo:
+    lines = ['["HS 20", "HS-20", "HS20"],',
+             '["HS 25", "HS-25", "HS25"],',
+             '["HS 30", "HS-30", "HS30"],',
+             '["HS 33", "HS-33", "HS33"],',
+             '["HS 35", "HS-35", "HS35"],',
+             '["HS 50", "HS-50", "HS50"],',
+             '["FinePix T200","FinePix T-200","FinePix T 200","T200","T-200","T 200"]',
+             '["FinePix T300","FinePix T-300","FinePix T 300","T300","T-300","T 300"]',
+             '["FinePix T350","FinePix T-350","FinePix T 350","T350","T-350","T 350"]',
+             '["FinePix T400","FinePix T-400","FinePix T 400","T400","T-400","T 400"]'
+             ]
+    for line in lines: 
+        
+        print(line.upper(), file = fo)
+
+
+
+# %% XP - series
+        
+        
+        
+url = 'https://en.wikipedia.org/wiki/Fujifilm_FinePix_XP-series'
+
+req = urllib.request.urlopen(url)
+article = req.read().decode()
+    
+with open('Fuji.html', 'w') as fo:
+    fo.write(article)
+        
+article = open('Fuji.html').read()
+soup = BeautifulSoup(article, 'html.parser')
+                    
+paragraphs = soup.find_all('ul')
+
+for paragraph in paragraphs:
+    with open('../../currated_data/fujifilm-models.json', 'a') as fo:
+        for lines in paragraph.find_all('li'):
+            if not lines:
+                continue
+            
+            name = lines.text.strip()
+            if "FinePix" in name:
+                name = name.replace("FinePix ","")
+                index_of_bracket = name.find('[')
+                name = name[:index_of_bracket]
+                name = name.replace(" ", "")
+                if name != "FujifilmFinePi" and name != "Fujifilmcamera":
+                    line = "[" + '"' + name + '","' + name[:2] + " "+ name[2:]+ '","' + name[:2] + "-"+ name[2:]  + '"],'
+                    print(line, file=fo)
+
+
+                
+os.remove("Fuji.html") 
+
+# %% z-series
+
+url = 'https://en.wikipedia.org/wiki/Fujifilm_FinePix_Z-series'
+
+req = urllib.request.urlopen(url)
+article = req.read().decode()
+    
+with open('Fuji.html', 'w') as fo:
+    fo.write(article)
+        
+article = open('Fuji.html').read()
+soup = BeautifulSoup(article, 'html.parser')
+                    
+paragraphs = soup.find_all('ul')
+
+for paragraph in paragraphs:
+    with open('../../currated_data/fujifilm-models.json', 'a') as fo:
+        for lines in paragraph.find_all('li'):
+            for name in lines.find_all('a'):
+                if not name:
+                    continue
+                name = name.text.strip()
+                if "FinePix" in name: 
+                    name = name.replace("FinePix ", "")
+                    name = name.replace(" ", "")
+                    if name != "FujifilmFinePix" and name != "Fujifilmcameras":
+                        print(name)
+                        line = "[" + '"' + name + '","' + name[:1] + " "+ name[1:]+ '","' + name[:1] + "-"+ name[1:]  + '"],'
+                        print(line,file=fo)
+
+                
+os.remove("Fuji.html")  
+
+
+# %% x-series
+
+
+
+
---
+++ scripts/label_and_match/label_fujifilm.py
@@ -0,0 +1,82 @@
+import json
+import os
+
+os.chdir('../..')
+
+sites = ['2013_camera_specs/www.walmart.com/',
+         '2013_camera_specs/buy.net/',
+         '2013_camera_specs/www.buzzillions.com/',
+         '2013_camera_specs/www.cambuy.com.au/',
+         '2013_camera_specs/buy.net/',
+         '2013_camera_specs/cammarkt.com/',
+         '2013_camera_specs/www.buzzillions.com/',
+         '2013_camera_specs/www.cambuy.com.au/',
+         '2013_camera_specs/www.camerafarm.com.au/',
+         '2013_camera_specs/www.canon-europe.com/',
+         '2013_camera_specs/www.ebay.com/',
+         '2013_camera_specs/www.eglobalcentral.co.uk/',
+         '2013_camera_specs/www.flipkart.com/',
+         '2013_camera_specs/www.garricks.com.au/',
+         '2013_camera_specs/www.gosale.com/',
+         '2013_camera_specs/www.henrys.com/',
+         '2013_camera_specs/www.ilgs.net/',
+         '2013_camera_specs/www.mypriceindia.com/',
+         '2013_camera_specs/www.pcconnection.com/',
+         '2013_camera_specs/www.price-hunt.com/',
+         '2013_camera_specs/www.pricedekho.com/',
+         '2013_camera_specs/www.priceme.co.nz/',
+         '2013_camera_specs/www.shopbot.com.au/',
+         '2013_camera_specs/www.shopmania.in/',
+         '2013_camera_specs/www.ukdigitalcameras.co.uk/',
+         '2013_camera_specs/www.walmart.com/',
+         '2013_camera_specs/www.wexphotographic.com/']
+
+with open('currated_data/fujifilm-models.json') as f:
+    nikon_models = json.load(f)
+
+notFound = []
+found = []
+for s in sites:
+    for filename in os.listdir(s):
+        with open(s + filename) as f:
+
+            # load the data
+            data = json.load(f)
+
+            # get the value
+            value = data['<page title>'].upper()
+
+            # skip if not nikon
+            if "FUJIFILM" not in value:
+                continue
+
+            match = True
+            for model in nikon_models:
+
+                match = False
+                for tag in model:
+                    # for coolpix we can try three different versions
+                    # COOLPIX, COOLPLIX, COOL PIX
+                   
+                    tag1 =  tag + " "
+                    if tag1 in value:
+                        match = True
+                        break
+                    
+                    tag2 = tag + "EXR"
+                    if tag2 in value:
+                        match = True
+                        break
+
+                if match:
+                    found.append(value + str(model))
+                    break
+
+            if not match:
+
+                print(value)
+                notFound.append(value)
+
+print("Not found : " + str(len(notFound)))
+print("Found : " + str(len(found)))
+
